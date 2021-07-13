30a21c0cafa6c0a3f364351c413cbd872985056c
navarichN <54509830+navarichN@users.noreply.github.com>
2021-07-13 19:06:38 +0200

    feat: add solution of the Highlight task

--- hometasks/src/components/Highlight/Highlight.js
+++ hometasks/src/components/Highlight/Highlight.js
@@ -40,14 +40,33 @@ function List(props) {
     return props.list.map(item => {
         switch (item.type) {
             case 'video':
-                return (
-                    <Video {...item} />
-                );
-
+                if(item.views >= 1000) {
+                    return (
+                        <Popular><Video {...item} /></Popular>
+                    );
+                } else if (item.views <= 100) {
+                    return (
+                        <New><Video {...item} /></New>
+                    );
+                }else {
+                    return (
+                        <Video {...item} />
+                    );
+                }
             case 'article':
-                return (
-                    <Article {...item} />
-                );
+                if(item.views >= 1000) {
+                    return (
+                        <Popular><Article {...item} /></Popular>
+                    );
+                } else if (item.views <= 100) {
+                    return (
+                        <New><Article {...item} /></New>
+                    );
+                }else {
+                    return (
+                        <Article {...item} />
+                    );
+                }
         }
     });
 };
